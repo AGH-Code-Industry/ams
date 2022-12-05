c581c2f468f8caf0fcee1c6d29ccf7a009c3978f
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2022-12-05 01:03:58 +0100

    Yet another enemies

---
+++ Assets/Scripts/Testing/Enemy/DummyEnemy.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DummyEnemy : MonoBehaviour {
+    private Animator _animator;
+    private float _startTime;
+    private float _avaitingTime;
+    private void Start() {
+        _animator = GetComponent<Animator>();
+        _startTime = Time.time;
+        _avaitingTime = Random.Range(2f, 5f);
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
