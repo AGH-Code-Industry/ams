ba8242c5362828825f37975b00a797ac980c0853
Zofia Mazur <zmazur@student.agh.edu.pl>
2022-12-09 17:47:06 +0100

    Collecting coins

---
+++ Assets/Scripts/Zbieranie monet/CollectingCoins/CollectingCoins.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections; 
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectingCoins : MonoBehaviour
+{
+    public int coins;
+
+    public void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Coin")
+        {
+            Debug.Log("Coin collected");
+            coins = coins + 1;
+            //Col.gameObject.SetActive(false);
+            Destroy(Col.gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Zbieranie monet/ThirdPersonMovement.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction.normalized * speed * Time.deltaTime);
+        }
+    }
+}
