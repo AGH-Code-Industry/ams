b541ca9cb89d5ae299eb290694e038cd34ea0152
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2022-12-14 21:32:52 +0100

    Działające menu, problemy z Image

--- Assets/Scripts/ScriptableObjects/ItemSO.cs
+++ Assets/Scripts/ScriptableObjects/ItemSO.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 namespace ScriptableObjects {
     public class ItemSO : ScriptableObject {
         [Header("Basic information")]
         public uint id;
+        public string itemName;
         public string description;
-        public Image icon;
+        [SerializeField] public Sprite icon;
     }
 }
 
