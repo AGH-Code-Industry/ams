0cc06db2778c667da89009182960eee1bf0a63fb
Zofia Mazur <zmazur@student.agh.edu.pl>
2022-12-16 14:04:21 +0100

    ekwipunek

---
+++ Assets/Scripts/Picking/InventoryManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Item = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+    
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+
+        }
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InvenotryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+            
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Picking/ItemController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public Item Item;
+    
+}
