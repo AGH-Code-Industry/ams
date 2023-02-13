d920a14a26528202969d321153be1a9ac1820554
Bodzio-2 <dungeonraiderpl@gmail.com>
2023-02-14 00:57:12 +0100

    General changes
    Rotation rework, SO cleanup, prefab creation streamline

--- Assets/Scripts/DamageSystem/NewSpellSystem/SpellTypes/Cone/coneInfo.cs
+++ Assets/Scripts/DamageSystem/NewSpellSystem/SpellTypes/Cone/coneInfo.cs
@@ -2,18 +2,22 @@ using DamageSystem.ReceiveDamage.Elementals.Elementals;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace DamageSystem.NewSpellSystem.SpellTypes.Cone
 {
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Spells/Cone", order = 4)]
     public class coneInfo : ScriptableObject
     {
-        public float cooldown;
-        public float castTime;
+       // public float cooldown;
+       // public float castTime;
         public float tickRate = 0.5f;
-        public float range;
-        public float spread;
-        public ParticleSystem particles;
+        //public float rangeY = 5.85f;
+        //public float spreadHorizontalX = 3.75f;
+        //public float spreadVerticalZ = 1.25f;
+        //[System.NonSerialized]
+        //public float rotationX = -90f;
+        public VisualEffectAsset particles;
         public List<AttackElemental> elementals;
     }
 }
