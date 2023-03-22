d818a98b80d0ab0fd6c3e11174dc8015aad26b86
Bodzio-2 <dungeonraiderpl@gmail.com>
2023-03-23 00:23:52 +0100

    Final improvements (for now) to the Laser Spell
    Removed hard coded implementation of handling VFX and moved it to an abstract class

--- Assets/Scripts/DamageSystem/NewSpellSystem/SpellTypes/Beam/beamSpellInfo.cs
+++ Assets/Scripts/DamageSystem/NewSpellSystem/SpellTypes/Beam/beamSpellInfo.cs
@@ -8,6 +8,7 @@ namespace DamageSystem.NewSpellSystem.SpellTypes.Beam
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Spells/Beam", order = 6)]
     public class beamSpellInfo : ScriptableObject
     {
+        //the time between every tick of damage of the beam
         public float tickRate = 0.5f;
         public List<AttackElemental> elementals;
     }
