e41a85851362a4cf1642080442619b2e7a4f214e
rubikon02 <rubikon02@gmail.com>
2023-03-28 20:48:02 +0200

    Fix double properties

--- Assets/Scripts/Testing/team4/Coin.cs
+++ Assets/Scripts/Testing/team4/Coin.cs
@@ -5,15 +5,8 @@ namespace team4.equipment {
 
     public class Coin : Item
     {
-        [SerializeField] private string _name = "Coin";
-        [SerializeField] private string _description = "This is a coin";
-
-        public override string itemName {
-            get => _name;
-        }
-        public override string description {
-            get => _description;
-        }
+        public override string itemName => "Coin";
+        public override string description => "This is a coin";
     }
 
 }
--- Assets/Scripts/Testing/team4/Item.cs
+++ Assets/Scripts/Testing/team4/Item.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 namespace team4.equipment {
     public abstract class Item : MonoBehaviour
     {
-        public abstract string itemName {get; }
-        public abstract string description {get; }
-        public virtual int stackSize {
-            get => Defaults.DEFAULT_STACK_SIZE;
-            set => stackSize = value;
-        }
+        public abstract string itemName { get; }
+        public abstract string description { get; }
+        public virtual int stackSize { get; set; } = Defaults.DEFAULT_STACK_SIZE;
 
         void Start()
         {
