c3d91d5a287db2bfece30c1c4f4edd1b8a23fc79
Spookyless <spookyless.info@gmail.com>
2023-04-25 20:38:09 +0200

    [#71] Pause works correctly, removed unnecessary invokes
    - Puase UI required double ESC press after exiting the menu through "Resume" button
    - Removed (commented out) delegates that change nothing in the behaviour of the script

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PauseController : MonoBehaviour
-{
-    public UnityEvent GamePaused;
-    public UnityEvent GameResumed;
+public class PauseController : MonoBehaviour {
+    //public UnityEvent GamePaused;
+    //public UnityEvent GameResumed;
 
     public GameObject PauseCanvas;
     public GameObject GameCanvas;
@@ -14,40 +13,40 @@ public class PauseController : MonoBehaviour
 
     public bool _isPaused = false;
 
-    void Start()
-    {
+    void Start() {
         PauseCanvas.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            _isPaused = !_isPaused;
-            if (_isPaused)
-            {
-                PauseGame();
-            }
-            else
-            {
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(_isPaused == true) {
                 ResumeGame();
+            } else {
+                PauseGame();
             }
         }
     }
-    public void PauseGame()
-    {
+
+    public void PauseGame() {
         Time.timeScale = 0;
-        GamePaused.Invoke();
+
+        //GamePaused?.Invoke();
+
         PauseCanvas.SetActive(true);
         GameCanvas.SetActive(false);
+
+        _isPaused = true;
     }
 
-    public void ResumeGame()
-    {
+    public void ResumeGame() {
         Time.timeScale = 1;
-        GameResumed.Invoke();
+
+        // GameResumed?.Invoke();
+
         PauseCanvas.SetActive(false);
         GameCanvas.SetActive(true);
+
+        _isPaused = false;
     }
 }
