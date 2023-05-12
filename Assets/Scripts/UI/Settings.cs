0f607ea71c8d6758384febeb37e6b846782941c7
ShatterPlayer <michalk101010@gmail.com>
2023-05-12 18:27:53 +0200

    Rewrote Settings class

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -7,18 +7,58 @@ using UnityEngine;
 
 public class Settings : MonoBehaviour
 {
-    public float volumeValue_;
-    public bool particlesOn_;
-    public int graphicQuality_;
-    public float sensitivityValue_;
-
-    public static Settings instance;
-    void Awake() {
-        instance = this;
-    }
-    void Start()
-    {
-
+    public class ResItem {
+    public int horizontal, vertical;
     }
 
+    public static List<ResItem> resolutions = new List<ResItem>() {
+        new ResItem() { horizontal = 1920, vertical = 1080 },
+        new ResItem() { horizontal = 1280, vertical = 720 },
+        new ResItem() { horizontal = 800, vertical = 600 }
+    };
+
+    public static bool fullscreen {
+        get { return PlayerPrefs.GetInt("isFullscreen", 0) == 1 ? true : false; }
+        set {
+            PlayerPrefs.SetInt("isFullscreen", value ? 1 : 0); 
+            Screen.fullScreen = value;
+            }
+    }
+
+    public static float volume {
+        get { return PlayerPrefs.GetFloat("volume", 1); }
+        set { PlayerPrefs.SetFloat("volume", value); }
+    }
+
+    public static int resolutionPreset {
+        get { return PlayerPrefs.GetInt("resolutionPreset", 0); }
+        set {
+            PlayerPrefs.SetInt("resolutionPreset", value);
+            Screen.SetResolution(resolutions[value].horizontal, resolutions[value].vertical, fullscreen);
+            }
+    }
+
+    public static int graphicQuality {
+        get { return PlayerPrefs.GetInt("graphicQuality", 2); }
+        set {
+            PlayerPrefs.SetInt("graphicQuality", value);
+            QualitySettings.SetQualityLevel(value);
+            }
+    }
+
+    public static float sensitivity {
+        get { return PlayerPrefs.GetFloat("sensitivity", 50); }
+        set { PlayerPrefs.SetFloat("sensitivity", value); }
+    }
+
+    public static bool particlesOn {
+        get { return PlayerPrefs.GetInt("particlesOn", 1) == 1 ? true : false; }
+        set { PlayerPrefs.SetInt("particlesOn", value ? 1 : 0); }
+    }
+
+    public static void ApplySettings() {
+        Screen.fullScreen = fullscreen;
+        Screen.SetResolution(resolutions[resolutionPreset].horizontal, resolutions[resolutionPreset].vertical, fullscreen);
+        QualitySettings.SetQualityLevel(graphicQuality);
+    }
 }
--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -11,7 +11,6 @@ public class SettingsMenu : MonoBehaviour
     public Toggle fullscreenToggle;
     public TextMeshProUGUI volumeText;
     public Slider volumeSlider;
-    public List<ResItem> resolutions = new List<ResItem>();
     private int selectedRes;
     public TextMeshProUGUI resolutionText;
     public TextMeshProUGUI ParticlesText;
@@ -21,65 +20,54 @@ public class SettingsMenu : MonoBehaviour
 
     void Start()
     {
-        
-        bool isFullscreen = PlayerPrefs.GetInt("isFullscreen", 0) == 1 ? true : false;
-        fullscreenToggle.isOn = isFullscreen;
-        Screen.fullScreen = isFullscreen;
+        fullscreenToggle.isOn = Settings.fullscreen;
 
-        Settings.instance.volumeValue_ = PlayerPrefs.GetFloat("Volume", 10);
-        volumeSlider.GetComponent<Slider>().value = Settings.instance.volumeValue_;
+        volumeSlider.GetComponent<Slider>().value = Settings.volume;
 
-        selectedRes = PlayerPrefs.GetInt("Resolution", 0);
-        resolutionText.text = resolutions[selectedRes].horizontal.ToString() + "x" + resolutions[selectedRes].vertical.ToString();
-        Screen.SetResolution(resolutions[selectedRes].horizontal, resolutions[selectedRes].vertical, fullscreenToggle.isOn);
+        selectedRes = Settings.resolutionPreset;
+        resolutionText.text = Settings.resolutions[selectedRes].horizontal.ToString() + "x" + Settings.resolutions[selectedRes].vertical.ToString();
+        Screen.SetResolution(Settings.resolutions[selectedRes].horizontal, Settings.resolutions[selectedRes].vertical, fullscreenToggle.isOn);
 
-        Settings.instance.particlesOn_ = (PlayerPrefs.GetInt("AreParticlesOn", 1) == 1 ? true : false);
         ParticlesApply();
         // Wł/Wył cząsteczki
 
-        Settings.instance.graphicQuality_ = PlayerPrefs.GetInt("Graphics", 2);
         GraphicApply();
 
-        Settings.instance.sensitivityValue_ = PlayerPrefs.GetFloat("Sensivity", 50);
-        sensitivitySlider.GetComponent<Slider>().value =  Settings.instance.sensitivityValue_;
+        sensitivitySlider.GetComponent<Slider>().value =  Settings.sensitivity;
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
-        Screen.fullScreen = isFullscreen;
-        PlayerPrefs.SetInt("isFullscreen", isFullscreen ? 1 : 0);
+        Settings.fullscreen = isFullscreen;
     }
 
     public void Volume(float value) {
-        Settings.instance.volumeValue_ = value;
+        Settings.volume = value;
         volumeText.text = value.ToString();
-        PlayerPrefs.SetFloat("Volume", value);
         // [Głośność] = value*[GłośnośćMax]/10
     }
 
     public void Resolution() {
         selectedRes++;
-        if(selectedRes >= resolutions.Count) {
+        if(selectedRes >= Settings.resolutions.Count) {
             selectedRes = 0;
             }
-        resolutionText.text = resolutions[selectedRes].horizontal.ToString() + "x" + resolutions[selectedRes].vertical.ToString();
+        resolutionText.text = Settings.resolutions[selectedRes].horizontal.ToString() + "x" + Settings.resolutions[selectedRes].vertical.ToString();
     }
 
     public void ApplyResolution() {
-        PlayerPrefs.SetInt("Resolution", selectedRes);
-        Screen.SetResolution(resolutions[selectedRes].horizontal, resolutions[selectedRes].vertical, fullscreenToggle.isOn);
+        Settings.resolutionPreset = selectedRes;
     }
 
     public void Particles() {
-        Settings.instance.particlesOn_ = !Settings.instance.particlesOn_;
+        Settings.particlesOn = !Settings.particlesOn;
         ParticlesApply();
         // Wł/Wył cząsteczki. Dodać to samo w metodzie Start()
     }
     
     public void ParticlesApply() {
 
-        PlayerPrefs.SetInt("AreParticlesOn", Settings.instance.particlesOn_ == true ? 1 : 0);
-        switch(Settings.instance.particlesOn_) {
+        switch(Settings.particlesOn) {
             case true:
                 ParticlesText.text = "On";
                 break;
@@ -90,14 +78,11 @@ public class SettingsMenu : MonoBehaviour
     }
 
     public void Graphics() {
-        Settings.instance.graphicQuality_++;
-        if(Settings.instance.graphicQuality_ > 2) {
-            Settings.instance.graphicQuality_ = 0;
-            }
+        Settings.graphicQuality = (Settings.graphicQuality + 1) % 3;
         GraphicApply();
     }
     public void GraphicApply() {
-        switch(Settings.instance.graphicQuality_) {
+        switch(Settings.graphicQuality) {
             case 0:
                 graphicText.text = "Low";
                 // set low
@@ -111,13 +96,11 @@ public class SettingsMenu : MonoBehaviour
                 // set high
                 break;
         }
-        PlayerPrefs.SetInt("Graphics", Settings.instance.graphicQuality_);
     }
 
      public void Sensivity(float value) {
-        Settings.instance.sensitivityValue_ = value;
+        Settings.sensitivity = value;
         SensivityText.text = value.ToString();
-        PlayerPrefs.SetFloat("Sensivity", value);
         // [Czułość] = value*[CzułośćMax]/100
     }
 
@@ -128,9 +111,4 @@ public class SettingsMenu : MonoBehaviour
     public void Back() {
         SceneManager.LoadScene("MenuStart");
     }
-}
-
-[System.Serializable]
-public class ResItem {
-    public int horizontal, vertical;
 }
