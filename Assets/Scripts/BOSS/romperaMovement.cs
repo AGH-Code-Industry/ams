1069a41e8cad3051ae5bf089752a30a45316935e
MBrosik <michal.bros1@gmail.com>
2023-06-10 20:40:17 +0200

    [#115] Add HealthBar

--- Assets/Scripts/BOSS/romperaMovement.cs
+++ Assets/Scripts/BOSS/romperaMovement.cs
@@ -1,62 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DamageSystem.ReceiveDamage.Elementals;
 
-public class romperaMovement : MonoBehaviour
+namespace BOSS
 {
-    private Renderer objectRenderer;
-    public Vector3 startingPosition;
-    public Transform player;
-    public float speed = 3f;
-    public float movRange = 16f;
-    private bool isMovingLeft = true;
-    private float periodCount = -0.5f;
-    private Vector3 positionBeforeTeleportaion;
+   public class romperaMovement : MonoBehaviour
+   {      
+      [SerializeField] private Damageable damageable;
+      private HealthBar healthBar;
+      private Renderer objectRenderer;
+      public Vector3 startingPosition;
+      public Transform player;
+      public float speed = 3f;
+      public float movRange = 16f;
+      private bool isMovingLeft = true;
+      private float periodCount = -0.5f;
+      private Vector3 positionBeforeTeleportaion;
 
-    // Start is called before the first frame update
-    void Start() {
-        objectRenderer = GetComponent<Renderer>();
-        startingPosition = transform.position;    
-    }
+      // Start is called before the first frame update
+      void Start()
+      {
+         objectRenderer = GetComponent<Renderer>();                  
+         startingPosition = transform.position;
+         healthBar = GameObject.Find("BossHPSlider").GetComponent<HealthBar>();
+         
+         healthBar.SetMaxHealth(damageable.entity.maxHealth);
+      }
 
 
-    // Update is called once per frame
-    void Update() {
+      // Update is called once per frame
+      void Update()
+      {
 
-        if (Time.time - periodCount >= 10) {
+         if (Time.time - periodCount >= 10)
+         {
             Teleportation();
-            periodCount = Time.time; 
-        }
+            periodCount = Time.time;
+         }
 
 
-        if (isMovingLeft) {
+         if (isMovingLeft)
+         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
 
-            if (Mathf.Abs(startingPosition.x - transform.position.x) >= movRange) {
-                isMovingLeft = false;
+            if (Mathf.Abs(startingPosition.x - transform.position.x) >= movRange)
+            {
+               isMovingLeft = false;
             }
-        }
-        else {
+         }
+         else
+         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
 
-            if (Mathf.Abs(startingPosition.x - transform.position.x) >= movRange) {
-                isMovingLeft = true;
+            if (Mathf.Abs(startingPosition.x - transform.position.x) >= movRange)
+            {
+               isMovingLeft = true;
             }
-        }
+         }
 
 
 
-        if(Mathf.Abs(transform.position.x - startingPosition.x) <= 0.1f) {
+         if (Mathf.Abs(transform.position.x - startingPosition.x) <= 0.1f)
+         {
             periodCount += 0.5f;
-        }
+         }
 
-    }
+      }
 
-    private void Teleportation() {
-        positionBeforeTeleportaion = transform.position;
-        positionBeforeTeleportaion.z += 38f;
-        transform.Translate(transform.position - positionBeforeTeleportaion);
-        transform.Rotate(0f, 180f, 0f);
+      private void Teleportation()
+      {
+         positionBeforeTeleportaion = transform.position;
+         positionBeforeTeleportaion.z += 38f;
+         transform.Translate(transform.position - positionBeforeTeleportaion);
+         transform.Rotate(0f, 180f, 0f);
 
-    }
+      } 
+      
+      /// <summary>
+      /// used in <see cref="Damageable.HealthUpdate"/>
+      /// </summary>   
+      public void SetHealth(int currentHP){
+         healthBar.SetHealth(currentHP);
+      }
+      
+      /// <summary>
+      /// used in <see cref="Damageable.Died"/>
+      /// </summary>      
+      public void OnDead(){
+         Destroy(gameObject);
+      }
+   }
 }
