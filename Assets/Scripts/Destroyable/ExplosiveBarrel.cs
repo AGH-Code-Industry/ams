ac67692f8df43a735c8a2168407a81a02944e6d4
Krzysztof Wrona <rubikon02@gmail.com>
2023-06-11 00:49:14 +0200

    Add barrel explosion effect and bloom post-processing

---
+++ Assets/Scripts/Destroyable/ExplosiveBarrel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrel : MonoBehaviour {
+    [SerializeField] private Collider collider;
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
