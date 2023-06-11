b516df432e756460da70d0de2f4bbc6490cdf967
Krzysztof Wrona <rubikon02@gmail.com>
2023-06-11 13:07:49 +0200

    Add barrel explosion sound

--- Assets/Scripts/Destroyable/ExplosiveBarrel.cs
+++ Assets/Scripts/Destroyable/ExplosiveBarrel.cs
@@ -6,10 +6,16 @@ namespace Destroyable {
     public class ExplosiveBarrel : MonoBehaviour, IExplosive {
         [SerializeField] private ExplosiveBarrelCollisionTrigger collisionTrigger;
         [SerializeField] private GameObject explosion;
+        private AudioSource audioSource;
+
+        private void Awake() {
+            audioSource = GetComponent<AudioSource>();
+        }
 
         public void Explode() {
             explosion.SetActive(true);
             collisionTrigger.gameObject.SetActive(false);
+            audioSource.Play();
         }
     }
 }
