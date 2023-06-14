29df9cdac190a18dfaa29c100112a184be44c296
Bodzio-2 <dungeonraiderpl@gmail.com>
2023-06-14 19:36:47 +0200

    Player spell cast fix + damage numbers face camera

--- Assets/Scripts/DamageSystem/DamageNumbers/DamagePopup.cs
+++ Assets/Scripts/DamageSystem/DamageNumbers/DamagePopup.cs
@@ -21,9 +21,12 @@ namespace DamageSystem.DamageNumbers
         private float disappearTimer;
         private Color textColor;
 
+        private Camera mainCam;
+
         private void Awake()
         {
             textMesh = transform.GetComponent<TextMeshPro>();
+            mainCam = Camera.main;
         }
 
         public void Setup(int damageAmount)
@@ -49,6 +52,13 @@ namespace DamageSystem.DamageNumbers
                     Destroy(gameObject);
                 }
             }
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 awayDirection = transform.position - mainCam.transform.position;
+            Quaternion awayRotation = Quaternion.LookRotation(awayDirection);
+            transform.rotation = Quaternion.Euler(awayRotation.eulerAngles.x, 0f, awayRotation.eulerAngles.z);
         }
     }
 }
