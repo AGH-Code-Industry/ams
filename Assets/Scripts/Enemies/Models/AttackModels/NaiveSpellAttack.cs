4da0f742920df0c3501dc2f2bf293391d354bfcc
Maciexpol <empieniazek@student.agh.edu.pl>
2023-06-16 14:20:13 +0200

    Generalized enemy spell casting

--- Assets/Scripts/Enemies/Models/AttackModels/NaiveSpellAttack.cs
+++ Assets/Scripts/Enemies/Models/AttackModels/NaiveSpellAttack.cs
@@ -1,21 +1,28 @@
-﻿using DamageSystem.NewSpellSystem.SpellTypes.Projectile;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using DamageSystem.NewSpellSystem.Core;
 
 namespace Enemies.Models.AttackModels {
-    public class ChairAttack : AttackModel{
-        
+    public class NaiveSpellAttack : AttackModel {
         [SerializeField] [Tooltip("Spell to attack")]
-        private SimpleProjectile spell;
+        private DamageSystem.NewSpellSystem.Core.Spell spell;
 
         private float _nextAvailableCastTime; 
         public override bool Attack() {
             if (!IsAbleToCast()) {
                 return false;
             }
+            
+            CastSpell();
+            return true;
+        }
+
+        private void CastSpell() {
             transform.LookAt(player);
             spell.Cast(transform);
-            _nextAvailableCastTime = Time.time + spell.GetCastTime();
-            return true;
+            _nextAvailableCastTime = Time.time + spell.GetCooldown();
         }
 
         private bool IsAbleToCast() {
