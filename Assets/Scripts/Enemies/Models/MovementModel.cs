6f140e92a435840013c4e1538feaebbf42b452b1
Maciexpol <empieniazek@student.agh.edu.pl>
2023-06-16 11:52:15 +0200

    MovementModel updated

--- Assets/Scripts/Enemies/Models/MovementModel.cs
+++ Assets/Scripts/Enemies/Models/MovementModel.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 namespace Enemies.Models {
     public abstract class MovementModel : MonoBehaviour {
         public abstract void Move(Vector3 destination);
+        public abstract bool IsDestinationReached();
         public abstract bool IsDestinationValid(Vector3 destination);
+        public abstract void Stop();
 
     }
 }
--- Assets/Scripts/Enemies/Models/MovementModels/EuclideanMovement.cs
+++ Assets/Scripts/Enemies/Models/MovementModels/EuclideanMovement.cs
@@ -12,14 +12,32 @@ namespace Enemies.Models.MovementModels {
             if (!IsDestinationValid(destination)) {
                 return;
             }
-            
             _navMeshAgent.Move(destination);
+        }
+
+        public override bool IsDestinationReached() {
+            if (!_navMeshAgent.pathPending) {
+                if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance) {
+                    if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public override bool IsDestinationValid(Vector3 destination) {
             var newPath = new NavMeshPath();
             _navMeshAgent.CalculatePath(destination, newPath);
             return newPath.status == NavMeshPathStatus.PathComplete;
+        }
+        
+        public override void Stop() {
+            _navMeshAgent.ResetPath();
+        }
+        
+        public void SetPath(NavMeshPath inputPath) {
+            _navMeshAgent.SetPath(inputPath);
         }
     }
 }
