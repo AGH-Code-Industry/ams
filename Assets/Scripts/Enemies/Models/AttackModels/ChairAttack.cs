f503f53d9977596153ef113add2e8e3fce1d9f39
Maciexpol <empieniazek@student.agh.edu.pl>
2023-06-16 13:57:14 +0200

    implemented chair attack

---
+++ Assets/Scripts/Enemies/Models/AttackModels/ChairAttack.cs
@@ -0,0 +1,25 @@
+﻿using DamageSystem.NewSpellSystem.SpellTypes.Projectile;
+using UnityEngine;
+
+namespace Enemies.Models.AttackModels {
+    public class ChairAttack : AttackModel{
+        
+        [SerializeField] [Tooltip("Spell to attack")]
+        private SimpleProjectile spell;
+
+        private float _nextAvailableCastTime; 
+        public override bool Attack() {
+            if (!IsAbleToCast()) {
+                return false;
+            }
+            transform.LookAt(player);
+            spell.Cast(transform);
+            _nextAvailableCastTime = Time.time + spell.GetCastTime();
+            return true;
+        }
+
+        private bool IsAbleToCast() {
+            return Time.time >= _nextAvailableCastTime;
+        }
+    }
+}
